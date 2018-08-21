c9534dbba5e2f32071207da20246e3036ca86e36
Mateus Alves <mateus.alves@locaweb.com.br>
2018-08-20 21:53:45 -0300

    Create auxiliary tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+
+  # Custom header helper
+  config.include Requests::HeaderHelpers, type: :request
 end
