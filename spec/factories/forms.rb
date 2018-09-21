754696b55daee368bf25c0b8e30d04749f3460d2
Mateus Alves <mateus.alves@locaweb.com.br>
2018-09-20 21:46:34 -0300

    Tests for forms

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :form do
     title { FFaker::Lorem.phrase }
-    description { FFaker::Lore.phrase }
+    description { FFaker::Lorem.phrase }
     user
     primary_color { "##{FFaker::Color.hex_code}" }
     enable { FFaker::Boolean.maybe }
