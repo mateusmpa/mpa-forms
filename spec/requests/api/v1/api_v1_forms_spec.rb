1912bb5383fb78862c916d88d28d4e347a8b36ca
Mateus Alves <mateus.alves@locaweb.com.br>
2018-11-01 20:21:17 -0300

    Add new tests to show and delete routes of the form

--- spec/requests/api/v1/api_v1_forms_spec.rb
+++ spec/requests/api/v1/api_v1_forms_spec.rb
@@ -39,16 +39,23 @@ RSpec.describe 'Api::V1::Forms', type: :request do
       context 'And it is enable' do
         before do
           @form = create(:form, user: @user, enable: true)
+          @question1 = create(:question, form: @form)
+          @question2 = create(:question, form: @form)
+
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
         end
 
         it 'returns 200' do
-          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           expect_status(200)
         end
 
         it 'returned form with right datas' do
-          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           expect(json).to eql(JSON.parse(@form.to_json))
+        end
+
+        it 'returned associated questions' do
+          expect(json['questions'].first).to eql(JSON.parse(@question1.to_json))
+          expect(json['questions'].last).to eql(JSON.parse(@question2.to_json))
         end
       end
 
@@ -193,6 +200,8 @@ RSpec.describe 'Api::V1::Forms', type: :request do
         context 'And user is the owner' do
           before do
             @form = create(:form, user: @user)
+            @question = create(:question, form: @form)
+
             delete "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           end
 
@@ -203,12 +212,16 @@ RSpec.describe 'Api::V1::Forms', type: :request do
           it 'form is deleted' do
             expect(Form.all.count).to eql(0)
           end
+
+          it 'associated question is deleted' do
+            expect(Question.all.count).to eql(0)
+          end
         end
 
         context 'And user is not the owner' do
           before do
             @form = create(:form)
-            delete "/api/v1/forms/#{form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+            delete "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           end
 
           it 'returns 403' do
