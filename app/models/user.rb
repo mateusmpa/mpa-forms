b02cc501462a6dae8ef054ede008e8b8eaa80458
Mateus Alves <mateus.alves@locaweb.com.br>
2018-12-21 17:43:19 -0200

    Include links and validations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :forms, dependent: :destroy
 end
